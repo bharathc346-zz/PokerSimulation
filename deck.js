41e259ba028ed8ffa67ff35004f6725e388911c8
bharathc <bharathc@gmail.com>
2017-01-08 17:52:32 -0800

    new file additions

--- deck.js
+++ deck.js
@@ -1,33 +1,12 @@
-var shuffle = require('shuffle-array');
+var cards = require('cards');
+ 
+// Create a new 52 card poker deck 
+var deck = new cards.PokerDeck();
+ 
+// Shuffle the deck 
+deck.shuffleAll();
+ 
+// Draw a card 
+var card = deck.draw();
 
-var SUITS =["H","D","S","C"];
-var RANKS =["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
-var deckSize = SUITS.length * RANKS.length;
-var deck = [];
-
-function createDeck(){
-	for(i =0; i<SUITS.length;i++){
-		for(j =0; j<RANKS.length;j++){
-			deck.push(RANKS[j]+SUITS[i]);
-		}
-	}
-}
-
-createDeck();
-shuffle(deck); 
-
-
-
-function dealTwo(){
-	var deal = []
-	for(i = 0; i<=1;i++){
-		deal.push(deck[i]);
-	}
-	deck.splice(0,2);
-	return deal;
-}
-
-console.log(deck);
-console.log(dealTwo());
-console.log(deck);
-
+console.log(card.unicodeString(); )
--- table.js
+++ table.js
@@ -1,2 +1,6 @@
 //Table has multiple players, Table Cards:(Flop, Turn, River), 
 var tableDeck = new Deck();
+
+function Table(pot){
+	this.pot = pot
+}
