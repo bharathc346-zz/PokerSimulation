2c4b466e4f4d6c1d38b0a02b61370e60bc5d8430
bharathc <bharathc@gmail.com>
2017-04-08 00:26:54 -0400

    Modified player hands; player hand now realistic with dealing

---
+++ cards.js
@@ -0,0 +1,66 @@
+function Cards() {
+	//cards making up deck 
+	this.deck = ['A♠', 'A♦', 'A♥', 'A♣', '2♠', '2♦', '2♥', '2♣', '3♠', '3♦', '3♥', '3♣', 
+				'4♠', '4♦', '4♥', '4♣', '5♠', '5♦', '5♥', '5♣', '6♠', '6♦', '6♥', '6♣',
+				'7♠', '7♦', '7♥', '7♣', '8♠', '8♦', '8♥', '8♣', '9♠', '9♦', '9♥', '9♣',
+				'10♠', '10♦', '10♥', '10♣', 'J♠', 'J♦', 'J♥', 'J♣', 'Q♠', 'Q♦', 'Q♥', 'Q♣',	
+				'K♠', 'K♦', 'K♥', 'K♣'];
+};
+
+//function to shuffle cards
+Cards.prototype.shuffle = function() {
+	//index 0-51
+	for(var i = 0; i < this.deck.length; i++) {
+		var randIndex = Math.floor(Math.random() * (52 - 0)) + 0;
+		var temp = this.deck[i];
+		this.deck[i] = this.deck[randIndex];
+		this.deck[randIndex] = temp;
+	}
+}
+
+//toString method to print deck of cards
+Cards.prototype.toString = function() {
+	var res = "[";
+	for (var i = 0; i < this.deck.length; i++ ) {
+		if(i !=this.deck.length-1) {
+			res+=this.deck[i]+", ";
+		}
+	}
+	res+= this.deck[this.deck.length-1]+"]";
+	return res;
+}
+
+//deal one card at a time
+Cards.prototype.deal = function() {
+	var res = this.deck.splice(0,1);
+	return res[0];
+}
+
+//reset cards to all 52
+Cards.prototype.reset = function() {
+	this.deck = ['A♠', 'A♦', 'A♥', 'A♣', '2♠', '2♦', '2♥', '2♣', '3♠', '3♦', '3♥', '3♣', 
+				'4♠', '4♦', '4♥', '4♣', '5♠', '5♦', '5♥', '5♣', '6♠', '6♦', '6♥', '6♣',
+				'7♠', '7♦', '7♥', '7♣', '8♠', '8♦', '8♥', '8♣', '9♠', '9♦', '9♥', '9♣',
+				'10♠', '10♦', '10♥', '10♣', 'J♠', 'J♦', 'J♥', 'J♣', 'Q♠', 'Q♦', 'Q♥', 'Q♣',	
+				'K♠', 'K♦', 'K♥', 'K♣'];
+}
+
+module.exports = Cards;
+
+//--------------------Testing-----------------------------------
+
+// var deck = new Cards();
+// deck.shuffle();
+// console.log('\n');
+// console.log(deck.toString());
+// console.log('\n');
+// console.log(deck.deal());
+// console.log(deck.deal());
+// console.log(deck.reset());
+// console.log('\n');
+// console.log(deck.toString());
+
+
+
+
+
--- table.js
+++ table.js
@@ -1,6 +1,63 @@
-//Table has multiple players, Table Cards:(Flop, Turn, River), 
-var tableDeck = new Deck();
+var Cards = require(__dirname+'/Cards.js');
+/*
+			--2--	--3--
+		--1--			--4--  where 6 is dealer, 1 is small blind, and 2 is big blind
+			--6--	--5--	
 
-function Table(pot){
-	this.pot = pot
+		
+*/
+function Table() {
+	this.commonCards = [];
+
+	//make table deck from cards class
+	this.deck = new Cards();
+	this.players = [1,2,3,4,5,6];
+	this.myPosition = Math.floor(Math.random() * (7 - 1)) + 1;
+	this.myHand = [];
+	this.othersHands = [];
+
+};
+
+//deal cards to players
+Table.prototype.dealToPlayers = function() {
+	console.log(this.deck.toString());
+	console.log("My Position: "+this.myPosition);
+	for(var i = 1; i<2*this.players.length+1;i++){
+		if(i%6 == this.myPosition || (this.myPosition == 6 && i%6 == 0)) 
+		{
+			this.myHand.push(this.deck.deal());
+		}
+		else{
+			this.othersHands.push(this.deck.deal());
+		}
+	}
 }
+
+//toString for myHand
+Table.prototype.myHandToString = function() {
+	var res = "["
+	for (var i = 0; i< this.myHand.length; i++) {
+		if(i!=this.myHand.length-1) {
+			res+= this.myHand[i]+", ";
+		}
+	}
+	res+=this.myHand[this.myHand.length-1]+"]";
+	return res;
+}
+
+
+module.exports = Table;
+
+
+
+
+
+//--------------------Testing-----------------------------------
+
+var table = new Table();
+table.deck.shuffle();
+table.dealToPlayers();
+console.log(table.myHandToString());  
+
+
+
