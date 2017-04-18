f5725472ffb4bac7d6bba26a5bb864633e606514
bharathc <bharathc@gmail.com>
2017-04-17 20:10:09 -0400

    Dealing working, and table evaluation begun

--- player.js
+++ player.js
@@ -1,29 +1,41 @@
-// Player has a name cards, an action, player capital, and a position
-
-var readlineSync = require('readline-sync'); 
-
-
-function Player(name, capital){
+function Player(name, money) {
 	this.nam = name;
-	this.capital = capital; 
+	this.money = money; 
+};
+
+//set name of the player
+Player.prototype.setName = function(str) {
+	this.nam = str;
 }
 
-
-Player.prototype.fold = function(){
-	//Putback player cards in the bottom of the deck
+//return name of player
+Player.prototype.getName = function() {
+	return this.nam;
 }
 
-Player.prototype.callBet = function(betAmount){
-	this.capital -= betAmount; 
+//setMoney for player
+Player.prototype.setMoney = function(x) {
+	this.money = x;
+} 
+
+//return money for player
+Player.prototype.getMoney = function() {
+	return this.money;
 }
 
-Player.prototype.raise = function(rasieAmount){
-	this.capital -= rasieAmount;
+//toString for player
+Player.prototype.toString = function() {
+	var res = this.nam+": 	"+this.money+"$";
+	return res;
 }
+
 module.exports = Player;
 
 
-var aids = new Player('Sharath', 40);
-console.log(aids);
-aids.callBet(20);
-console.log('Money after calling: '+aids.capital);
+
+//--------------------Testing-----------------------------------
+// var player = new Player("Player 1",40);
+// console.log(player.toString());
+
+
+
--- table.js
+++ table.js
@@ -1,8 +1,11 @@
 var Cards = require(__dirname+'/Cards.js');
+var Player = require(__dirname+'/Player.js')
 /*
-			--2--	--3--
-		--1--			--4--  where 6 is dealer, 1 is small blind, and 2 is big blind
-			--6--	--5--	
+				--3--	--4--
+			--2--			--5--
+		--1--				 	--6--  where 9 is dealer, 1 is small blind, and 2 is big blind
+			--9--	--8--	--7--	
+				
 
 		
 */
@@ -11,19 +14,25 @@ function Table() {
 
 	//make table deck from cards class
 	this.deck = new Cards();
-	this.players = [1,2,3,4,5,6];
+	this.players = [];
 	this.myPosition = Math.floor(Math.random() * (7 - 1)) + 1;
 	this.myHand = [];
 	this.othersHands = [];
 
 };
 
+//create a number (numpPlayer) of players with amMoney
+Table.prototype.createPlayers = function(numPlayers, amMoney) {
+	for(var i = 0; i<numPlayers;i++) {
+		 this.players.push((new Player("P"+(i+1),amMoney)));
+	}
+}
+
 //deal cards to players
 Table.prototype.dealToPlayers = function() {
-	console.log(this.deck.toString());
-	console.log("My Position: "+this.myPosition);
 	for(var i = 1; i<2*this.players.length+1;i++){
-		if(i%6 == this.myPosition || (this.myPosition == 6 && i%6 == 0)) 
+		if(i%this.players.length == this.myPosition || 
+			(this.myPosition == this.players.length && i%this.player.length == 0)) 
 		{
 			this.myHand.push(this.deck.deal());
 		}
@@ -45,6 +54,48 @@ Table.prototype.myHandToString = function() {
 	return res;
 }
 
+//evaluate the position of the player (Helper method)
+//-1 is early, 0 is middle, 1 is late
+Table.prototype.evalPos = function() {
+	if(this.myPosition <= 9 && this.myPosition >=7) {
+		return 1;
+	}
+	else if(this.myPosition <=6 && this.myPosition >=4) {
+		return 0;
+	}
+	else {
+		return -1;
+	}
+}
+
+
+//evaluate starting hand
+Table.prototype.evalHand = function() {
+	var posType = this.evalPos();
+	var nums = [];
+	var suits = [];
+	var suited = false; 
+	var paired = false;
+
+	//go through 2 elements
+	for(var i = 0; i<this.myHand.length;i++) {
+		suits.push(this.myHand[i][0]);
+		nums.push(this.myHand[i][1]);
+	}
+
+	
+	if(nums[0] == nums[1]) {
+		paired = true; 
+	}
+
+	if(suits[0] == suits[1]) {
+		suited = true; 
+	}
+	console.log(suited);
+	console.log(paired);
+
+}
+
 
 module.exports = Table;
 
@@ -56,8 +107,12 @@ module.exports = Table;
 
 var table = new Table();
 table.deck.shuffle();
+
+table.createPlayers(9,40);
+console.log(table.players);
 table.dealToPlayers();
-console.log(table.myHandToString());  
+console.log(table.myHandToString());
+table.evalHand();
 
 
 
